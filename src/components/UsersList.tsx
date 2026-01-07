b711de63fd565d3b0db7570406391340e41b6bd1
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-07 19:32:48 +0600

    add user

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,11 +1,34 @@
 import { useEffect, useState } from "react";
 import type { User } from "../types/user/user";
-import { getUser } from "../services/api";
+import { createUser, getUser } from "../services/api";
 
 const UsersList = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [newUser, setNewUser] = useState<Omit<User, "id">>({
+    name: "",
+    username: "",
+    email: "",
+    address: {
+      street: "",
+      suite: "",
+      city: "",
+      zipcode: "",
+      geo: {
+        lat: "",
+        lng: "",
+      },
+    },
+    phone: "",
+    website: "",
+    company: {
+      name: "",
+      catchPhrase: "",
+      bs: "",
+    },
+  });
+
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -21,6 +44,38 @@ const UsersList = () => {
     };
     fetchUsers();
   }, []);
+
+  const handleCreateUser = async () => {
+    try {
+      
+      const createdUser = await createUser(newUser);
+      setUsers([...users, createdUser]);
+      setNewUser({
+        name: "",
+        username: "",
+        email: "",
+        address: {
+          street: "",
+          suite: "",
+          city: "",
+          zipcode: "",
+          geo: {
+            lat: "",
+            lng: "",
+          },
+        },
+        phone: "",
+        website: "",
+        company: {
+          name: "",
+          catchPhrase: "",
+          bs: "",
+        },
+      });
+    } catch {
+      setError("Failed to Crate New User");
+    } 
+  };
 
   if (loading)
     return (
@@ -40,7 +95,42 @@ const UsersList = () => {
     );
   return (
     <div className="">
-      
+      <div className="bg-white shadow-2xl px-4 py-10 sm:px-6 mb-10 ">
+        <h1 className="text-xl font-medium mb-4">Add new User</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <input
+            onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+            value={newUser.name}
+            className="block w-full rounded-md border border-indigo-300 shadow-sm  focus:border-indigo-500 focus: ring-indigo-500 text-lg py-2 px-2"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            value={newUser.username}
+            onChange={(e) =>
+              setNewUser({ ...newUser, username: e.target.value })
+            }
+            className="block w-full rounded-md border border-indigo-300 shadow-sm  focus:border-indigo-500 focus: ring-indigo-500 text-lg py-2 px-2"
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+            value={newUser.email}
+            className="block w-full rounded-md border border-indigo-300 shadow-sm  focus:border-indigo-500 focus: ring-indigo-500 text-lg py-2 px-2"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="mt-5">
+          <button
+            onClick={handleCreateUser}
+            className="inline-flex justify-center px-5 py-1.5 rounded-md border-transparent bg-indigo-600 hover:bg-indigo-700 font-medium text-white"
+          >
+            Add User
+          </button>
+        </div>
+      </div>
 
       <div className="bg-white shadow-2xl px-4 py-10 sm:px-6">
         <h1 className="text-lg mb-10 font-medium py-1.5">Users</h1>
