46de23b21bd7f593d7f0f3b9178f93a07931f7c0
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-07 17:31:28 +0600

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-indigo-100 py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-center mb-8">User Dashboard</h1>
+        <p>user list</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
