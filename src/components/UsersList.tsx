7e689298aab1184fafb271f130a7d95b07d73961
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-07 19:04:13 +0600

    create user function

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,4 +1,4 @@
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import type { User } from "../types/user/user";
 import { getUser } from "../services/api";
 
@@ -21,7 +21,6 @@ const UsersList = () => {
     };
     fetchUsers();
   }, []);
- 
 
   if (loading)
     return (
@@ -40,37 +39,39 @@ const UsersList = () => {
       </div>
     );
   return (
-    <div className="bg-white shadow-2xl">
-      <div className="px-4 sm:px-6 mb-10 border-b border-indigo-200">
-        <h1 className="text-lg font-medium py-1.5">Users</h1>
-      </div>
-      <ul className="divide-y divide-indigo-200">
-        {users.map((user) => (
-          <li key={user.id} className="px-4 sm:px-6 py-4">
-            <div className="flex items-center">
-              <div className="flex-1 min-w-0">
-                <p>
-                  <span className="font-bold text-lg truncate">Name:</span>{" "}
-                  {user?.name}
-                </p>
-                <p>
-                  <span className="font-bold text-lg truncate">Email:</span>{" "}
-                  {user?.email}
-                </p>
-              </div>
+    <div className="">
+      
 
-              <div className="flex gap-2">
-                <button className="inline-flex px-4 py-1.5 items-center border border-transparent text-xs font-medium rounded-md text-indigo-500 bg-indigo-200 hover:bg-indigo-100 hover:text-indigo-400 cursor-pointer">
-                  Update
-                </button>
-                <button className="inline-flex px-4 py-1.5 items-center border border-transparent text-xs font-medium rounded-md text-red-500 bg-red-200 hover:bg-red-100 hover:text-red-400 cursor-pointer">
-                  Delete
-                </button>
+      <div className="bg-white shadow-2xl px-4 py-10 sm:px-6">
+        <h1 className="text-lg mb-10 font-medium py-1.5">Users</h1>
+        <ul className="divide-y divide-indigo-200">
+          {users.map((user) => (
+            <li key={user.id} className="px-4 sm:px-6 py-4">
+              <div className="flex items-center">
+                <div className="flex-1 min-w-0">
+                  <p>
+                    <span className="font-bold text-lg truncate">Name:</span>{" "}
+                    {user?.name}
+                  </p>
+                  <p>
+                    <span className="font-bold text-lg truncate">Email:</span>{" "}
+                    {user?.email}
+                  </p>
+                </div>
+
+                <div className="flex gap-2">
+                  <button className="inline-flex px-4 py-1.5 items-center border border-transparent text-xs font-medium rounded-md text-indigo-500 bg-indigo-200 hover:bg-indigo-100 hover:text-indigo-400 cursor-pointer">
+                    Update
+                  </button>
+                  <button className="inline-flex px-4 py-1.5 items-center border border-transparent text-xs font-medium rounded-md text-red-500 bg-red-200 hover:bg-red-100 hover:text-red-400 cursor-pointer">
+                    Delete
+                  </button>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,4 @@
+
 import type { User } from "../types/user/user";
 
 const API_URL = "https://jsonplaceholder.typicode.com/users";
@@ -6,6 +7,20 @@ export const getUser = async (): Promise<User[]> => {
   const res = await fetch(API_URL);
   if (!res.ok) {
     throw new Error("Failed to fetch user");
+  }
+  return res.json();
+};
+
+export const createUser = async (user: Omit<User, "id">): Promise<User> => {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  if (!res.ok) {
+    throw Error("Failed to create new user");
   }
   return res.json();
 };
